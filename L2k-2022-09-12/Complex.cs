67e5dd28f1d05c40ec7006e2c198f15aff2f3203
Sergey Makletsov <smaklets@kpfu.ru>
2022-09-30 17:13:26 +0300

    Добавьте файлы проекта.

---
+++ L2k-2022-09-12/Complex.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L2k_2022_09_12
+{
+    class Complex
+    {
+        private static int count = 0;
+        private int index;
+        private static double EPSILON = 1e-25;
+        public static int Count
+        {
+            get => count;
+        }
+
+        private double im;
+
+        public double Re { get; set; }
+
+        public double Im
+        {
+            get => im;
+            set => im = value;
+        }
+
+        public double roIm => im;
+
+        public double Abs => Math.Sqrt(Re * Re + Im * Im);
+
+        public Complex()
+        {
+            count++;
+            index = count;
+        }
+
+        public Complex(double re, double im)
+        {
+            Re = re;
+            this.im = im;
+            count++;
+            index = count;
+        }
+
+        public Complex(Complex other)
+        {
+            Re = other.Re;
+            im = other.im;
+            count++;
+            index = count;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(index);
+            sb.Append("):");
+            if (Math.Abs(Re) > 10 * Double.Epsilon ||
+                Math.Abs(Im) < 10 * Double.Epsilon)
+                sb.Append(Re);
+            if (Math.Abs(Im) > 10 * Double.Epsilon)
+            {
+                sb.Append(Im > 0 ? " + " : " - ");
+                if (Math.Abs(Math.Abs(Im) - 1) > 10 * Double.Epsilon) 
+                    sb.Append(Math.Abs(Im));
+                sb.Append("i");
+            }
+            return sb.ToString();
+        }
+
+        public Complex Plus(Complex other) =>
+            new (Re + other.Re, Im + other.Im);
+
+        public static Complex operator+(Complex z1, Complex z2) =>
+            new (z1.Re + z2.Re, z1.Im + z2.Im);
+
+        public static Complex operator -(Complex z1, Complex z2) =>
+            new(z1.Re - z2.Re, z1.Im - z2.Im);
+
+        public static Complex operator -(Complex z1) =>
+            new(z1.Re, -z1.Im);
+
+        public static bool operator ==(Complex it, Complex other)
+        {
+            return Math.Abs(it.Re - other.Re) < EPSILON && 
+                   Math.Abs(it.Im - other.Im) < EPSILON;
+        }
+
+        public static bool operator !=(Complex it, Complex other) => !(it == other);
+
+        public override bool Equals(object? obj)
+        {
+            /*if (obj == null) return false;
+            if (obj.GetType() != typeof(Complex)) return false;
+            Complex x = (Complex)obj;*/
+            if (obj is Complex x)
+                return Math.Abs(Re - x.Re) < EPSILON && Math.Abs(Im - x.Im) < EPSILON;
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Re, Im);
+        }
+    }
+}
---
+++ L2k-2022-09-12/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Reflection.Metadata;
+using System.Xml;
+using L2k_2022_09_12;
+
+Complex z1 = new Complex();
+Complex z2 = new Complex(3, -5);
+Complex z3 = new Complex(z2);
+Complex z4 = z2;
+z2.Im = -1;
+Console.WriteLine(z1.Abs);
+Console.WriteLine(z2.Abs);
+Console.WriteLine(z3.Abs);
+Console.WriteLine(z4.Abs);
+Console.WriteLine(z1);
+Console.WriteLine(z2);
+Console.WriteLine(z3);
+Console.WriteLine(z4);
+Complex z5 = z2.Plus(z3);
+Console.WriteLine(z5); 
+Complex z6 = z4 + z2;
+Console.WriteLine(z6);
+Console.WriteLine(Complex.Count);
+Console.WriteLine(-z6);
+z6 += z4;
+Console.WriteLine(z6);
+
+int? i1 = null;
+if (i1 != null)
+{
+    int i11 = (int)i1;
+    int i2 = i11 + 1;
+}
+
+if (i1 is { } i12)
+{
+    int i2 = i12 + 1;
+}
+
+var i3 = 1 + i1 ?? 0;
+Complex? zz = new Complex(3, 5);
+Console.WriteLine(zz?.Re.ToString() ?? "NULL");
+int xx = default;
+Console.WriteLine(xx);
+
+/*int[] mas = {3, 5, 7};
+int[][] m2 = new int[10][];
+for (int i = 0; i < 10; i++)
+{
+    m2[i] = new int[i + 1];
+}
+
+m2[0][1] = 333;
+
+int[,] m3 = new int[10, 20];
+m3[0, 1] = 333;
+*/
+Days dd = Days.Monday;
+Console.WriteLine(dd);
+Days dd2 = Days.Sunday | Days.Saturday;
+Console.WriteLine(dd2);
+
+Days dd3 = Days.Sunday | Days.Monday;
+Console.WriteLine(dd3);
+[Flags]
+enum Days
+{
+    Monday    = 0b_00000001,
+    Tuesday   = 0b_00000010,
+    Wednesday = 0b_00000100,
+    Thursday  = 0b_00001000,
+    Friday    = 0b_00010000,
+    Saturday  = 0b_00100000,
+    Sunday    = 0b_01000000,
+    WeekEnd   = Saturday | Sunday
+}
